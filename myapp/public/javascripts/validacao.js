872082d05d7f5600d32eadea980984c4a00412d9
Piêtro <pietrobragaaquino@gmail.com>
2020-06-26 20:33:39 -0300

    ultima atualizacao

--- myapp/public/javascripts/validacao.js
+++ myapp/public/javascripts/validacao.js
@@ -1,37 +1,12 @@
-function cadastrarPessoas(){
-
-
-
-
+function validacao(){
 	var nomehtml = $("#nome").val()
 	var sobrenomehtml = $("#sobrenome").val()
 	var cpfhtml = $("#cpf").val()
 	var telefonehtml = $("#telefone").val()
 	var enderecohtml = $("#endereco").val()
-
 	if(nomehtml == ""||sobrenomehtml==""||cpfhtml==""||telefonehtml==""||enderecohtml==""){
 		alert("Você precisa preencher todos os campos!")
-		return;
+		return false;
 	}
-
-
-
-
-	var html = "<tr>";
-	html += "<td>"+nomehtml+"</td>";
-	html += "<td>"+sobrenomehtml+"</td>";
-	html += "<td>"+cpfhtml+"</td>";
-	html += "<td>"+telefonehtml+"</td>";
-	html += "<td>"+enderecohtml+"</td>";
-	html += "</tr>";
-	$("#registros").html($("#registros").html()+html);
-
-
-
-	$(".campo").val("");
-
-
-	alert("Cadastro concluido!")
-
-	document.getElementById("nome").focus();
+	return true;
 }
