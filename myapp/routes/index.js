00535497ac6e442197968843fb05899d4a073010
Piêtro <pietrobragaaquino@gmail.com>
2020-06-26 20:44:56 -0300

    tudo ok

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,6 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/cadastrar-pessoa', function(req, res, next) {
+
+
+
+	var nome = req.param("nome")
+	console.log(nome)
+
   res.render('index', { title: 'cadastrar-pessoa' });
 });
 module.exports = router;
