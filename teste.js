29f01f8dc2fe9b89d64f1c6b9b598f0cae5f1548
Piêtro <pietrobragaaquino@gmail.com>
2020-06-26 14:14:36 -0300

    testando javascript na minha máquina denovo

--- teste.js
+++ teste.js
@@ -1,1 +1,7 @@
-console.log("teste - nodejs rodando na minha máquina")
+console.log("Rodando javascript na minha máquina como se fosse servidor utilizando NODEJS!")
+
+a = 20
+b = 30
+
+c = a + b
+console.log("resultado = " + c) 
