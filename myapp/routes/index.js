80190d682eb8512b72fe8b68ecd84f453e27b8f0
Piêtro <pietrobragaaquino@gmail.com>
2020-06-27 13:26:53 -0300

    Agora tudo está funcionando perfeitamente

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+pessoas = []
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', pessoas: pessoas });
 });
+
+router.post('/cadastrar-pessoa', function(req, res, next) {
+	pessoa = {
+		nome: req.body.nome,
+		sobrenome: req.body.sobrenome,
+		telefone: req.body.telefone
+	}
+	pessoas.push(pessoa)
+	console.log(pessoa)
+  res.render('index', { title: 'cadastrar-pessoa',pessoas: pessoas });
+});
+
 
 module.exports = router;
