71b20ed18ef5c9a796f6d95ca7a7ee5f851101f6
Piêtro <pietrobragaaquino@gmail.com>
2020-07-02 17:24:23 -0300

    coloquei jquery e prendendo começar ajax

--- myapp/public/javascripts/validacao.js
+++ myapp/public/javascripts/validacao.js
@@ -1,9 +1,10 @@
+
 function validacao(){
-	var nome= document.getElementById("nomecompleto").value
-	var cpf = document.getElementById("cpf").value
-	var telefone = document.getElementById("telefone").value
-	var endereco = document.getElementById("endereco").value
-	var email = document.getElementById("email").value
+	var nome= $("#nomecompleto").val()
+	var cpf = $("#cpf").val()
+	var telefone = $("#telefone").val()
+	var endereco = $("#endereco").val()
+	var email = $("#email").val()
 	if(nome==""||cpf==""||telefone==""||endereco==""||email==""){
 		alert("Não deixe espaços em branco!")
 		return false;
