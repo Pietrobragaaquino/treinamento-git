7077bd78e0f4b3e249f8c3fa56f07ac8c726a5e5
Piêtro <pietrobragaaquino@gmail.com>
2020-07-08 15:19:06 -0300

    cliente ok

--- myapp2/public/javascripts/validacao.js
+++ myapp2/public/javascripts/validacao.js
@@ -8,3 +8,27 @@ function validacao(){
 		return true;
 	}
 }
+
+function validacaoCliente(){
+	var nome= $("#nome").val()
+	var telefone = $("#telefone").val()
+	var endereco = $("#endereco").val()
+	var email = $("#email").val()
+	if(nome==""||telefone==""||endereco==""||email==""){
+		alert("Não deixe espaços em branco!")
+		return false;
+	}else if (isNaN(telefone)){
+		alert("Telefone deve ser um número!")
+		return false;
+	}else{
+		return true;
+	}
+}
+function temcerteza(){
+	x = confirm("Tem certeza que deseja excluir?"); 
+	if (x == true){
+		return true;
+	}else{
+		return false;
+	}
+}
--- myapp2/routes/index.js
+++ myapp2/routes/index.js
@@ -1,10 +1,53 @@
 var express = require('express');
 var router = express.Router();
 var mensagem = ""
+var clientes =[]
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { mensagem: mensagem });
 });
+
+
+router.get('/cliente', function(req, res, next) {
+
+
+
+var sql = "SELECT * FROM `myapp2`.`cliente`";
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+    con.query(sql, function (err, clientes) {
+      if (err) throw err;
+      console.log("retornando clientes");
+  res.render('cliente', { mensagem: mensagem, clientes: clientes });
+});
+    });
+  });
+
+router.get('/funcionario', function(req, res, next) {
+  res.render('funcionario', { mensagem: mensagem});
+});
+
+router.get('/produto', function(req, res, next) {
+  res.render('produto', { mensagem: mensagem});
+});
+
+router.get('/compra', function(req, res, next) {
+  res.render('compra', { mensagem: mensagem });
+});
+
+
+
+
+
 
 
 
@@ -31,6 +74,61 @@ router.post('/home', function(req, res, next) {
 	});
   });
 });
+
+
+
+router.post('/cadastrar-cliente', function(req, res, next) {
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO `myapp2`.`cliente` ( `telefone`, `email`, `nome`, `endereco`) VALUES ('"+req.body.telefone+"', '"+req.body.email+"', '"+req.body.nome+"', '"+req.body.endereco+"');";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("inserido cliente");
+    var sql = "SELECT * FROM `myapp2`.`cliente`";
+    con.query(sql, function (err, clientes) {
+      if (err) throw err;
+      console.log("retornando clientes");
+      res.render('cliente', { clientes: clientes });
+      });
+    });
+  });
+});
+
+
+router.get('/excluir-cliente', function(req, res, next) {
+  var mysql = require('mysql');
+  var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  });
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+      var sql = "DELETE FROM `myapp2`.`cliente` WHERE (`idcliente` = '"+req.query.idcliente+"');";
+      con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("deletado pessoa");
+      var sql = "SELECT * FROM `myapp2`.`cliente`";
+      con.query(sql, function (err, clientes) {
+      if (err) throw err;
+      console.log("retornado pessoas");
+      res.render('cliente', { clientes: clientes });
+      });
+    });
+  });
+});
+
 
 
 module.exports = router;
