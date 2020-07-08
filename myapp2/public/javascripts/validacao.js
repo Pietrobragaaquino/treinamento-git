80983b3f1ce8b86cb0f76582899108891958d957
Piêtro <pietrobragaaquino@gmail.com>
2020-07-08 18:55:05 -0300

    falta pouca coisa agora

--- myapp2/public/javascripts/validacao.js
+++ myapp2/public/javascripts/validacao.js
@@ -56,11 +56,57 @@ function validacaoProduto(){
 		return true;
 	}
 }
+
+function validacaoVenda(){
+	var quantidade = $("#datavenda").val()
+	var idcliente = $("#idcliente").val()
+	var idfuncionario = $("#idfuncionario").val()
+	var idproduto = $("#idproduto").val()
+	var datavenda = $("#datavenda").val()
+	if(datavenda==""||idcliente==""||idfuncionario==""||idproduto==""){
+		alert("Não deixe espaços em branco!")
+		return false;
+	}else if (isNaN(idcliente)&&isNaN(idfuncionario)&&isNaN(idproduto)&&datavenda.length!=10){
+		alert("Os identificadores deverão ser números inteiros e a data deve ter o formato : YYYY-MM-DD!")
+		return false;
+	}else{
+		return true;
+	}
+}
+
 function temcerteza(){
 	x = confirm("Tem certeza que deseja excluir?"); 
 	if (x == true){
 		return true;
 	}else{
 		return false;
+	}
+}
+
+function validacaoClienteBusca(){
+	var nomeclientebusca = $("#nomeclientebusca").val()
+	if(nomeclientebusca==""){
+		alert("Digite o nome do cliente para buscar!")
+		return false;
+	}else{
+		return true;
+	}
+}
+function validacaoProdutoBusca(){
+	var nomeprodutobusca = $("#nomeprodutobusca").val()
+	if(nomeprodutobusca==""){
+		alert("Digite o nome do produto para buscar!")
+		return false;
+	}else{
+		return true;
+	}
+}
+function validacaoFuncionarioBusca(){
+	var nomefuncionariobusca = $("#nomefuncionariobusca").val()
+	if(nomefuncionariobusca==""){
+		alert("Digite o nome do funcionário para buscar!")
+		return false;
+	}else{
+		return true;
 	}
 }
--- myapp2/routes/index.js
+++ myapp2/routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var mensagem = ""
 var clientes =[]
+var clientesbusca=[]
+var produtosbusca=[]
+var funcionariosbusca=[]
+var vendas=[]
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { mensagem: mensagem });
@@ -77,9 +81,174 @@ con.connect(function(err) {
 
 
 
-router.get('/compra', function(req, res, next) {
-  res.render('compra', { mensagem: mensagem });
+router.get('/venda', function(req, res, next) {
+  var mysql = require('mysql');
+  var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  });
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+  var sql = "SELECT * FROM `myapp2`.`venda`";
+    con.query(sql, function (err, vendas) {
+      if (err) throw err;
+      console.log("retornando vendas");
+      res.render('venda', { produtosbusca: produtosbusca , clientesbusca:clientesbusca , funcionariosbusca: funcionariosbusca,vendas: vendas });
 });
+});
+});
+
+
+router.get('/buscarnomecliente', function(req, res, next) {
+  var mysql = require('mysql');
+  var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  });
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+      var sql = "SELECT * FROM `myapp2`.`cliente`";
+      con.query(sql, function (err, clientesbusca) {
+      if (err) throw err;
+      console.log("retornando clientes");
+
+
+      var sql = "SELECT * FROM myapp2.cliente WHERE nome LIKE '%"+req.query.nomeclientebusca+"%';"
+      con.query(sql, function (err, clientesbusca) {
+      if (err) throw err;
+      console.log("retornado busca por nome");
+
+
+      res.render('venda', {  clientesbusca: clientesbusca, produtosbusca: produtosbusca , funcionariosbusca: funcionariosbusca,vendas: vendas });
+
+
+      });
+    });
+  });
+});
+
+
+router.get('/buscarnomeproduto', function(req, res, next) {
+  var mysql = require('mysql');
+  var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  });
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+      var sql = "SELECT * FROM `myapp2`.`produto`";
+      con.query(sql, function (err, produtosbusca) {
+      if (err) throw err;
+      console.log("retornando produtos");
+
+
+      var sql = "SELECT * FROM myapp2.produto WHERE nome LIKE '%"+req.query.nomeprodutobusca+"%';"
+      con.query(sql, function (err, produtosbusca) {
+      if (err) throw err;
+      console.log("retornado busca por nome");
+
+
+      res.render('venda', {  produtosbusca: produtosbusca , clientesbusca:clientesbusca , funcionariosbusca: funcionariosbusca,vendas: vendas });
+
+
+      });
+    });
+  });
+});
+
+
+router.get('/buscarnomefuncionario', function(req, res, next) {
+  var mysql = require('mysql');
+  var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  });
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+      var sql = "SELECT * FROM `myapp2`.`funcionario`";
+      con.query(sql, function (err, funcionariosbusca) {
+      if (err) throw err;
+      console.log("retornando funcionarios");
+
+
+      var sql = "SELECT * FROM myapp2.funcionario WHERE nome LIKE '%"+req.query.nomefuncionariobusca+"%';"
+      con.query(sql, function (err, funcionariosbusca) {
+      if (err) throw err;
+      console.log("retornado busca por nome");
+
+
+      res.render('venda', {  produtosbusca: produtosbusca , clientesbusca:clientesbusca , funcionariosbusca: funcionariosbusca,vendas: vendas });
+
+
+      });
+    });
+  });
+});
+
+
+
+router.post('/cadastrar-venda', function(req, res, next) {
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO `myapp2`.`venda` ( `idcliente`, `idfuncionario`, `idproduto`, `quantidade`, `datavenda`) VALUES ('"+req.body.idcliente+"', '"+req.body.idfuncionario+"', '"+req.body.idproduto+"', '"+req.body.quantidade+     "', '"+    req.body.datavenda   +"');";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("inserido venda");
+    var sql = "SELECT * FROM `myapp2`.`venda`";
+    con.query(sql, function (err, vendas) {
+      if (err) throw err;
+      console.log("retornando vendas");
+      res.render('venda', { produtosbusca: produtosbusca , clientesbusca:clientesbusca , funcionariosbusca: funcionariosbusca,vendas: vendas });
+      });
+    });
+  });
+});
+
+router.get('/excluir-venda', function(req, res, next) {
+  var mysql = require('mysql');
+  var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  });
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+      var sql = "DELETE FROM `myapp2`.`venda` WHERE (`idvenda` = '"+req.query.idvenda+"');";
+      con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("deletado venda");
+      var sql = "SELECT * FROM `myapp2`.`venda`";
+      con.query(sql, function (err, vendas) {
+      if (err) throw err;
+      console.log("retornado vendas");
+      res.render('venda', {produtosbusca: produtosbusca , clientesbusca:clientesbusca , funcionariosbusca: funcionariosbusca,vendas: vendas });
+      });
+    });
+  });
+});
+
+
+
 
 
 
